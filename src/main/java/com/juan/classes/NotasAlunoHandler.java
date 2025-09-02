c8bc19514f302b0661c598eedfe3736db38806ac
JuanWillian <juanwcaraujo28@gmail.com>
2025-09-02 11:20:25 -0300

    refactor: mudando de map pra lista e retornando o aluno na lista

--- src/main/java/com/juan/classes/NotasAlunoHandler.java
+++ src/main/java/com/juan/classes/NotasAlunoHandler.java
@@ -4,8 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
@@ -14,53 +14,59 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class NotasAlunoHandler extends AbstractHandler{
-	
-	@Override
-	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-		System.out.println("entrou no handler");
-		response.setContentType("application/json");
-		response.setStatus(HttpServletResponse.SC_OK);
-		String path = request.getRequestURI(); 
-		String[] partes = path.split("/");
-		String idAluno = partes.length > 3 ? partes[3] : null;
-		response.getWriter().print("HelloWorld");
-		Map<String, Double> map = procurarNotasAluno(idAluno);
-		response.getWriter().print(map.get(idAluno));
-		baseRequest.setHandled(true);
-	}
-	
-	public Map<String, Double> procurarNotasAluno(String idAluno) {
-		File file = new File(criarSubpastas()+File.separator+"notas.csv");
-		if(file.mkdirs()) {
-		try(BufferedReader br = new BufferedReader(new FileReader(file))){
-			Map<String, Double> map = new HashMap<>();
-			String line = br.readLine();
-			while(line != null) {
-				if(line.split(";").equals(idAluno)) {
-					String[] vet = line.split(";");
-					map.put(vet[0], Double.parseDouble(vet[1]));
-					System.out.println(line);
-				}
-			line = br.readLine();
-			return map;
-			}
-		}catch(IOException e) {
-			System.out.println(e.getMessage());
-		}}
-		return null;
-	
-	}
-	
-	
-	//criar pra diminuir a qtd de codigo que vai ter caso eu queira ler mais arquivos
+public class NotasAlunoHandler extends AbstractHandler {
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        System.out.println("entrou no handler");
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        String path = request.getRequestURI();
+        String[] partes = path.split("/");
+        String idAluno = partes.length > 3 ? partes[3] : null;
+//        response.getWriter().print("HelloWorld");
+        List<Aluno> list = procurarNotasAluno(idAluno);
+        response.getWriter().println(list.stream().map(Aluno::getNota));
+        baseRequest.setHandled(true);
+    }
+
+    public List<Aluno> procurarNotasAluno(String idAluno) {
+        File arquivo = criarArquivoCsv();
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+            List<Aluno> list = new ArrayList<>();
+            String line = br.readLine();
+            while (line != null) {
+                String[] vet;
+                vet = line.split(",");
+                if (vet[0].equals(idAluno)) {
+                    list.add(new Aluno(Integer.parseInt(vet[0]), vet[1], Double.parseDouble(vet[2])));
+                }
+                line = br.readLine();
+            }
+
+            return list;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+
+    }
+
+
+    //criar pra diminuir a qtd de codigo que vai ter caso eu queira ler mais arquivos
 //	public void lerArquivos() {
 //		
 //	}
-	
-	public File criarSubpastas() {
-		return new File("Alunos"+File.separator+"Notas");
-	}
-	
+
+    public File criarArquivoCsv() {
+        //talvez possa virar atributo ou algo assim
+        File caminhoBase = new File(File.separator + "home" + File.separator + "juan" + File.separator + "Documentos");
+        if (caminhoBase.mkdirs()) System.out.println("Criado");
+        ;
+        File subpasta = new File(File.separator + "PortalAluno" + File.separator + "Alunos" + File.separator + "Notas");
+        if (subpasta.mkdirs()) System.out.println("CRIADO");
+        return new File(caminhoBase + "" + subpasta + File.separator + "notas.csv");
+
+    }
+
 }
