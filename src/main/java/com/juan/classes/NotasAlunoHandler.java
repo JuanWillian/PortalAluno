bb043e69196032b7f1b71919a387763a3d578b34
JuanWillian <juanwcaraujo28@gmail.com>
2025-09-02 12:46:12 -0300

    refactor: mudando para retornar um aluno com uma lista de notas e verificando a uri

--- src/main/java/com/juan/classes/NotasAlunoHandler.java
+++ src/main/java/com/juan/classes/NotasAlunoHandler.java
@@ -5,7 +5,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.DoubleStream;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
@@ -20,31 +22,31 @@ public class NotasAlunoHandler extends AbstractHandler {
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         System.out.println("entrou no handler");
         response.setContentType("application/json");
+        String[] uri = request.getRequestURI().split("/");
+        if (uri.length < 3) {
+            response.getWriter().println("O id do aluno não foi enviado");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        }
         response.setStatus(HttpServletResponse.SC_OK);
-        String path = request.getRequestURI();
-        String[] partes = path.split("/");
-        String idAluno = partes.length > 3 ? partes[3] : null;
-//        response.getWriter().print("HelloWorld");
-        List<Aluno> list = procurarNotasAluno(idAluno);
-        response.getWriter().println(list.stream().map(Aluno::getNota));
+        String idAluno = uri[3];
+        Aluno aluno = procurarNotasAluno(idAluno);
+        response.getWriter().println("As notas de "+aluno.getNome()+" foram: "+aluno.getNotas());
         baseRequest.setHandled(true);
     }
 
-    public List<Aluno> procurarNotasAluno(String idAluno) {
+    public Aluno procurarNotasAluno(String idAluno) {
         File arquivo = criarArquivoCsv();
         try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
-            List<Aluno> list = new ArrayList<>();
             String line = br.readLine();
             while (line != null) {
                 String[] vet;
                 vet = line.split(",");
                 if (vet[0].equals(idAluno)) {
-                    list.add(new Aluno(Integer.parseInt(vet[0]), vet[1], Double.parseDouble(vet[2])));
+                    return new Aluno(Integer.parseInt(vet[0]), vet[1], Arrays.asList(Double.parseDouble(vet[2]), Double.parseDouble(vet[3])));
                 }
                 line = br.readLine();
             }
 
-            return list;
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
@@ -55,7 +57,7 @@ public class NotasAlunoHandler extends AbstractHandler {
 
     //criar pra diminuir a qtd de codigo que vai ter caso eu queira ler mais arquivos
 //	public void lerArquivos() {
-//		
+//
 //	}
 
     public File criarArquivoCsv() {
